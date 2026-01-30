cb2c691db39e31bbe1a9e5b50750b189d768f515
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-01-30 21:27:43 +0530

    make model better, optimization

--- linear-regression/multiple-linear-regression.py
+++ linear-regression/multiple-linear-regression.py
@@ -6,7 +6,7 @@ import numpy as np
 
 data = pd.read_csv("housing.csv")
 bedrooms = data['total_bedrooms']
-data = data.drop(["total_bedrooms", "ocean_proximity", "longitude", "latitude", "median_income", "median_house_value"], axis=1)
+data = data.drop(["total_bedrooms", "ocean_proximity"], axis=1)
 
 
 data["bedrooms"] = bedrooms
@@ -16,44 +16,26 @@ data = data.dropna()
 ## Loss Function 
 
 def pridict_y(X, w, b):
-    m = X.shape[0]
-
-    y_hat = np.zeros(m)
-
-    for i in range(m):
-        y_hat[i] = np.dot(w, X.iloc[i]) + b
-
-    return y_hat
+    return np.dot(X, w)  + b
 
 
-def loss_function(X, y, w, b, data): 
-    m = len(data) 
+def loss_function(X, y, w, b): 
+    m = len(y) 
 
     y_hat = pridict_y(X, w, b)
-    sum = np.sum((y_hat-y)**2)
 
-    res = sum / (2. * m)
-
-    return res
+    return np.sum((y_hat-y)**2) / (2 * m)
     
 
 ## Gradient Descent 
 
 def gradient_descnet_step(X, y, w, b):
     m = X.shape[0]
-    n = X.shape[1]
+    y_hat = pridict_y(X, w, b)
+    err = y_hat- y
 
-    dj_dw = np.zeros(n)
-    dj_db = 0
-
-    for i in range(n):
-        err = np.dot(w, X.iloc[i]) + b - y[i]
-
-        dj_dw += err * X.iloc[i]
-        dj_db += err
-
-    dj_dw /= m
-    dj_db /= m
+    dj_dw = (1/m) * np.dot(X.T, err) 
+    dj_db = (1/m) * np.sum(err)
 
     return dj_dw, dj_db
 
@@ -64,8 +46,8 @@ def gradient_descnet(X, y,w , b,epoches=1000, alpha=0.001):
     for i in range(epoches):
 
         if i % 50 == 0:
-            print("Epoche:", i)
-            history.append((i,loss_function(X, y,w, b, data)))
+            print("Epoche:", i, ", Error:", loss_function(X, y, w, b))
+            history.append((i,loss_function(X, y,w, b)))
 
         dj_dw, dj_db = gradient_descnet_step(X, y, w,b)
         w = w - dj_dw * alpha
@@ -76,10 +58,17 @@ def gradient_descnet(X, y,w , b,epoches=1000, alpha=0.001):
 
 X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
-w = np.zeros(data.shape[-1]-1) 
+
+
+X = (X - np.mean(X, axis=0)) / np.std(X, axis=0)
+X = X.to_numpy()
+y = y.to_numpy()
+
+w = np.zeros(X.shape[1])
 b = 0.0
-alpha = 0.0003
-epoches = 3000
+alpha = 0.1
+epoches = 1000
+
 
 w, b = gradient_descnet(X, y, w, b, epoches, alpha) 
 
@@ -115,3 +104,5 @@ plt.legend()
 plt.grid(True, linestyle='--', alpha=0.6)
 
 plt.show()
+
+print(data)
